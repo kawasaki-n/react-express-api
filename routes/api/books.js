5b6288a65e1bbcace4fe49a4ceb5c7d657550681
Naoya Kawasaki <qqks23sd@gmail.com>
2020-11-22 17:20:05 +0900

    use mysql2/promise

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,19 +1,37 @@
-var express = require('express');
+const express = require('express');
 const mysql = require('mysql2')
-var router = express.Router();
+const router = express.Router();
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOSTNAME,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD
-});
+const connection_param = {
+  "host": process.env.DB_HOSTNAME,
+  "database": process.env.DB_NAME,
+  "user": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD
+}
 
 router.get('/', function(req, res, next) {
-  const sql = "select * from book order by id"
-  con.query(sql, (e, r, f) => {
-    res.json(r)
-  });
+  const sql = "select * from book order by id";
+  let con;
+  try {
+    con = mysql.createConnection(connection_param);
+    con.query(sql, (e, r, f) => {
+      if (e) {
+        throw e;
+      }
+      res.json(r);
+    });
+  } catch (error) {
+    console.log(error);
+    con.rollback();
+    return {
+      "status": "error",
+      "error": "fail to fetch data."
+    }
+  } finally {
+    if (con && con.connection) {
+      con.end();
+    }
+  }
 });
 
 module.exports = router;
